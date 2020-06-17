1e20f5861835b8749ee83028fa32799785590610
marcos <marcosolive@hotmail.com>
2020-06-16 21:32:42 -0300

    update todo

--- todo.js
+++ todo.js
@@ -1,1 +1,42 @@
-alert('Funcionou!')
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer café',
+    'Estudar JS',
+    'Acessar comunidade rocketseat'
+];
+
+
+function renderTodos() {
+    listElement.innerHTML='';
+    for (todo of todos) {
+        var todoElemente = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.setAttribute('href','#');
+
+        linkElement.appendChild(linkText);
+
+        todoElemente.appendChild(todoText);
+        todoElemente.appendChild(linkElement);
+
+        listElement.appendChild(todoElemente);
+    }
+}
+
+renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+}
+
+buttonElement.onclick = addTodo;
